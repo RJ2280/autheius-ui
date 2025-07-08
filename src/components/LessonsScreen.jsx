ec126398f3e3c4854b64edd5fa7398d58655e940
Robert Allen <robbienosebest@gmaol.com>
2025-07-08 13:27:13 -0400

    changed files: src/data/lessons.json

---
+++ src/components/LessonsScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LessonCenter from '../components/LessonCenter.jsx';
+
+const LessonsScreen = () => {
+  return (
+    <div>
+      <LessonCenter />
+    </div>
+  );
+};
+
+export default LessonsScreen;
